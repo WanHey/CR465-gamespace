f6f771f92275b6917e73f0e5f456962764a06cf6
WanHey <wan-lan.he@etud.polymtl.ca>
2025-10-26 17:22:47 +0000

    First

--- scripts/settings.php
+++ scripts/settings.php
@@ -23,4 +23,17 @@ $databases['default']['default'] = array (
 );
 $settings['hash_salt'] = 'FmzFZyZxGzZdrB4GZ7-CyAYc5w8_jg-_8a05HKWcZKqhWjFUHfnc4qQ4rQalRFIj10GaA_Es5w';
 $settings['config_sync_directory'] = 'sites/default/files/config_mikRNdRAdE26WRCm5bLklgrVtb2sc1F7IQnknDqOkg38XXKYIzo96jBN60m-NGALYgS3CdDVbA/sync';
-*/
+*/$databases['default']['default'] = array (
+  'database' => 'database',
+  'username' => 'spaceuser',
+  'password' => 'mycoolsecret',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'LxCvXWyLUoznDtSjWM6bXnZlOByCIi_oVNh5czBPrAlDQBfRE4ZAJw-qN8nmlQLJgJumw_nB0g';
+$settings['config_sync_directory'] = 'sites/default/files/config__1-LmJAQVwHvj38uja9JPiMJAkzsMbnLDyuchf38XIMwhiLTQw2NTcI-ElgBoHuBdtYfurl1RA/sync';
